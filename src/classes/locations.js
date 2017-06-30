dc80ece8757d9c4f05dff92cfdc6189db979c2a8
Mitch <iammitchellreeves@gmail.com>
2017-06-30 14:54:21 +1000

    Fourth commit

--- src/classes/locations.js
+++ src/classes/locations.js
@@ -1,7 +1,42 @@
 export const locations = [
-  {lat: -31.98505269, lng: 115.860457},
-  {lat: -33.955, lng: 115.07500},
-  {lat: -25.363, lng: 131.044},
-  {lat: -37.816023, lng: 144.965792}
+  {
+    coordinates: {
+      lat: -31.98505269,
+      lng: 115.860457,
+    },
+    projectName: "Water Project",
+    site: "Perth",
+    content: "(last visited June 22, 2009).",
+    url: "https://en.wikipedia.org/w/index.php?title=Uluru",
+  },
+  {
+    coordinates: {
+      lat: -33.955,
+      lng: 115.07500,
+    },
+    projectName: "Water Project 2",
+    site: "Place 2",
+    content: "(last visited June 22, 2009).",
+    url: "https://en.wikipedia.org/w/index.php?title=Uluru",
+  },
+  {
+    coordinates: {
+      lat: -25.363,
+      lng: 131.044,
+    },
+    projectName: "Water Project 3",
+    site: "Place 3",
+    content: "(last visited June 22, 2009).",
+    url: "https://en.wikipedia.org/w/index.php?title=Uluru",
+  },
+  {
+    coordinates: {
+      lat: -37.816023,
+      lng: 144.965792,
+    },
+    projectName: "Water Project 4",
+    site: "Place 4",
+    content: "(last visited June 22, 2009).",
+    url: "https://en.wikipedia.org/w/index.php?title=Uluru",
+  }
 ]
-
--- src/services/map.js
+++ src/services/map.js
@@ -1,13 +1,14 @@
 import { mapStyles } from '../classes/mapStyles'
-import { locations } from '../classes/locations'
-import { contentStrings } from '../classes/contentStrings'
+import { locations} from '../classes/locations'
 
 export default function initMap() {
   let google = window.google
 
+  let firstLocation = locations[1]
+
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: locations[1],
+    center: firstLocation.coordinates,
     styles: mapStyles
   });
 
@@ -15,25 +16,50 @@ export default function initMap() {
 
   var icons = {
     marker: {
-      icon: 'locationmarkerblue.png'
+      icon: './images/locationmarkerblue.png'
     }
   }
 
 
   var infoWindow = new google.maps.InfoWindow({
-    disableAutoPan: true
+    disableAutoPan: true,
+    maxWidth: 200,
+    maxHeight: 50,
   });
 
   var markers = locations.map(function(location, i) {
     var marker = new google.maps.Marker({
       disableAutoPan: true,
-      position: location,
+      position: location.coordinates,
       icon: icons['marker'].icon,
-      label: labels[i % labels.length]
+      label: labels[i % labels.length],
     });
 
+    let contentString = `
+      <div>
+        <div class="content">
+            <div class="siteNotice"></div>
+        </div>
+        
+        <h1 class="firstHeading">
+            ${location.projectName}
+        </h1>
+        <div class="bodyContent">
+          
+          <p class="locationSite">
+            <b>${location.site}</b>
+          </p>
+          <p>
+            ${location.content}
+            <a href="${location.url}"></a>
+          </p>
+        </div>
+ 
+      </div>
+    `
+
     marker.addListener('mouseover', function() {
-      infoWindow.setContent(contentStrings[i % labels.length])
+      infoWindow.setContent(contentString)
       infoWindow.open(map, marker);
     });
 
@@ -41,5 +67,8 @@ export default function initMap() {
   });
 
   var markerCluster = new window.MarkerClusterer(map, markers,
-    {imagePath:'markerclusterer.png'});
+    {imagePath:'./images/m'});
 }
+
+// marker label text colour
+// infowindow sizes
